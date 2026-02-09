f3322458bf02f153bafdeadf4629c41b77b0076c
ahmed a <aa87@tuta.io>
2026-02-09 09:13:52 -0500

    Implemented TEST007 Basic Integer Interpolation (PASSING)

--- backend/mumps/MIOTPLT.m
+++ backend/mumps/MIOTPLT.m
@@ -1,83 +1,66 @@
 MIOTPLT
 	;
-	D TEST001,TEST002,TEST003,TEST004,TEST005
+	D TEST001,TEST002,TEST003,TEST004,TEST005,TEST006,TEST007
 	Q
-	;
 TEST001
 	NEW HDR S HDR="[MIOTPL][TEST001][No Interpolation]"
 	NEW DESC S DESC=HDR_"[Mustache-free templates should render as-is]"
-	NEW TOK,ERR,CONF,CTX,OUT
-	D COMPILE^MIOTPL2($$UES("Hello from {Mustache}!\n"),.TOK,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[COMPILE] "_HDR)
-	DO EVAL^MIOTPL2(.TOK,.CONF,.CTX,.OUT,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[EVAL]"_DESC)
-	DO EQ^MIOTASSERT(OUT,$$UES("Hello from {Mustache}!\n"),"[RENDER]"_DESC)
+	NEW TEMPLATE S TEMPLATE=$$UES("Hello from {Mustache}!\n")
+	NEW EXPECTED S EXPECTED=$$UES("Hello from {Mustache}!\n")
+	NEW CTX
+	D RUNTEST1(HDR,DESC,TEMPLATE,EXPECTED,.CTX)
 	QUIT
 TEST002
 	NEW HDR S HDR="[MIOTPL][TEST002][Basic Interpolation]"
 	NEW DESC S DESC=HDR_"[Unadorned tags should interpolate content into the template.]"
-	NEW TOK,ERR,CONF,CTX,OUT
-	D COMPILE^MIOTPL2($$UES("Hello, {{subject}}!\n"),.TOK,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[COMPILE]"_HDR)
+	NEW TEMPLATE S TEMPLATE=$$UES("Hello, {{subject}}!\n")
+	NEW EXPECTED S EXPECTED=$$UES("Hello, world!\n")
+	NEW CTX
 	SET CTX("subject")="world"
-	DO EVAL^MIOTPL2(.TOK,.CONF,.CTX,.OUT,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[EVAL]"_DESC)
-	DO EQ^MIOTASSERT(OUT,$$UES("Hello, world!\n"),"[RENDER]"_DESC)
+	D RUNTEST1(HDR,DESC,TEMPLATE,EXPECTED,.CTX)
 	QUIT
 TEST003
 	NEW HDR S HDR="[MIOTPL][TEST003][No Re-interpolation]"
 	NEW DESC S DESC=HDR_"[Interpolated tag output should not be re-interpolated.]"
-	NEW TOK,ERR,CONF,CTX,OUT
-	D COMPILE^MIOTPL2("{{template}}: {{planet}}",.TOK,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[COMPILE]"_HDR)
+	NEW TEMPLATE S TEMPLATE="{{template}}: {{planet}}"
+	NEW EXPECTED S EXPECTED="{{planet}}: Earth"
+	NEW CTX
 	SET CTX("template")="{{planet}}"
 	SET CTX("planet")="Earth"
-	DO EVAL^MIOTPL2(.TOK,.CONF,.CTX,.OUT,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[EVAL]"_DESC)
-	DO EQ^MIOTASSERT(OUT,"{{planet}}: Earth","[RENDER]"_DESC)
+	D RUNTEST1(HDR,DESC,TEMPLATE,EXPECTED,.CTX)
 	QUIT
 TEST004
 	NEW HDR S HDR="[MIOTPL][TEST004][HTML Escaping]"
 	NEW DESC S DESC=HDR_"[Basic interpolation should be HTML escaped..]"
-	NEW TOK,ERR,CONF,CTX,OUT
-	D COMPILE^MIOTPL2("These characters should be HTML escaped: {{forbidden}}",.TOK,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[COMPILE]"_HDR)
-	SET CTX("forbidden")="& "" < >"
-	DO EVAL^MIOTPL2(.TOK,.CONF,.CTX,.OUT,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[EVAL]"_DESC)
-	DO EQ^MIOTASSERT(OUT,"These characters should be HTML escaped: &amp; &quot; &lt; &gt;","[RENDER]"_DESC)
+	NEW TEMPLATE S TEMPLATE="These characters should be HTML escaped: {{forbidden}}"
+	NEW EXPECTED S EXPECTED="These characters should be HTML escaped: &amp; &quot; &lt; &gt;"
+	NEW CTX SET CTX("forbidden")="& "" < >"
+	D RUNTEST1(HDR,DESC,TEMPLATE,EXPECTED,.CTX)
 	QUIT
-	;
 TEST005
 	NEW HDR S HDR="[MIOTPL][TEST005][Triple Mustache]"
 	NEW DESC S DESC=HDR_"[Triple mustaches should interpolate without HTML escaping.]"
 	NEW TEMPLATE S TEMPLATE="These characters should not be HTML escaped: {{{forbidden}}}"
 	NEW EXPECTED S EXPECTED="These characters should not be HTML escaped: & "" < >"
-	;
 	NEW CTX SET CTX("forbidden")="& "" < >"
-	;
-	NEW TOK,ERR,CONF,OUT
-	D COMPILE^MIOTPL2(TEMPLATE,.TOK,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[COMPILE]"_HDR)
-	DO EVAL^MIOTPL2(.TOK,.CONF,.CTX,.OUT,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[EVAL]"_DESC)
-	DO EQ^MIOTASSERT(OUT,EXPECTED,"[RENDER]"_DESC)
+	D RUNTEST1(HDR,DESC,TEMPLATE,EXPECTED,.CTX)
 	QUIT
-	;
 TEST006
 	NEW HDR S HDR="[MIOTPL][TEST006][Ampersand]"
-	NEW DESC S DESC=HDR_"[These characters should not be HTML escaped: {{&forbidden}}]"
+	NEW DESC S DESC=HDR_"[Ampersand should interpolate without HTML escaping.]"
 	NEW TEMPLATE S TEMPLATE="These characters should not be HTML escaped: {{{forbidden}}}"
 	NEW EXPECTED S EXPECTED="These characters should not be HTML escaped: & "" < >"
-	;
 	NEW CTX SET CTX("forbidden")="& "" < >"
-	;
-	NEW TOK,ERR,CONF,OUT
-	D COMPILE^MIOTPL2(TEMPLATE,.TOK,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[COMPILE]"_HDR)
-	DO EVAL^MIOTPL2(.TOK,.CONF,.CTX,.OUT,.ERR)
-	DO OK^MIOTASSERT('$D(ERR),"[EVAL]"_DESC)
-	DO EQ^MIOTASSERT(OUT,EXPECTED,"[RENDER]"_DESC)
+	D RUNTEST1(HDR,DESC,TEMPLATE,EXPECTED,.CTX)
+	QUIT
+TEST007
+	NEW HDR S HDR="[MIOTPL][TEST007][Basic Integer Interpolation]"
+	NEW DESC S DESC=HDR_"[Integers should interpolate seamlessly.]"
+	NEW TEMPLATE S TEMPLATE="These characters should not be HTML escaped: {{{forbidden}}}"
+	NEW EXPECTED S EXPECTED="These characters should not be HTML escaped: & "" < >"
+	NEW CTX 
+	SET CTX("forbidden")="& "" < >"
+	D RUNTEST1(HDR,DESC,TEMPLATE,EXPECTED,.CTX)
 	QUIT
 UES(X)
 	N POS,Y,START
@@ -87,6 +70,15 @@ UES(X)
 	. I 'POS S Y=Y_$E(X,START,$L(X)),POS=$L(X) I 1
 	. E  S Y=Y_$E(X,START,POS-2)_$$REALCHAR($E(X,POS),X,.POS)
 	Q Y
+RUNTEST1(HDR,DESC,TEMPLATE,EXPECTED,CTX)
+	NEW TOK,ERR,CONF,OUT
+	D COMPILE^MIOTPL2(TEMPLATE,.TOK,.ERR)
+	DO OK^MIOTASSERT('$D(ERR),"[COMPILE]"_HDR)
+	DO EVAL^MIOTPL2(.TOK,.CONF,.CTX,.OUT,.ERR)
+	DO OK^MIOTASSERT('$D(ERR),"[EVAL]"_DESC)
+	DO EQ^MIOTASSERT(OUT,EXPECTED,"[RENDER]"_DESC)
+	Q
+	;
 REALCHAR(C,X,POS)
 	N OPOS
 	I C="""" Q """"
